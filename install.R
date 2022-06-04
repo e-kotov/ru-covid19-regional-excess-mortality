60489c6eebc14b5fd4eb4b849d2691a5c6885d5c
Egor Kotov <e-kotov@users.noreply.github.com>
2022-06-04 15:57:36 +0300

    fix install.R

--- install.R
+++ install.R
@@ -20,8 +20,6 @@ if( !"renv" %in% installed.packages()[,'Package'] )  { install.packages("renv") 
 if( !"here" %in% installed.packages()[,'Package'] )  { install.packages("here") }
 if( !"rmarkdown" %in% installed.packages()[,'Package'] )  { install.packages("rmarkdown") }
 
-source(here::here("R", "package_list.R"))
-
 invisible({capture.output({
   renv::install(package_list, type = "binary")
 })})
